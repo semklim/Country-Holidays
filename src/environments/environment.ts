768ac4a08f6c7fc12376c03f0d6fbb101ad569a7
semklim <romanklimashenko@gmail.com>
2024-09-07 03:40:08 +0300

    feat: fix prod environment for vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/dot-notation */
 export const environment = {
-  apiBaseUrl: process.env['API_BASE_URL'],
+  // apiBaseUrl: process.env['API_BASE_URL'],
+  // For deploy to vercel
+  apiBaseUrl: 'https://date.nager.at',
 };
